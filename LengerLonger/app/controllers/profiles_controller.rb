6aee74f8e1d907b30fa932a8bc7d93bfee514ea1
CorainChicago <chaysmagan@gmail.com>
2015-11-15 18:05:51 -0600

    start profile creation

--- LengerLonger/app/controllers/profiles_controller.rb
+++ LengerLonger/app/controllers/profiles_controller.rb
@@ -1,4 +1,10 @@
 class ProfilesController < ApplicationController
+  
+  def new
+    
+  end
+
+
   def edit
    @profile = Profile.find_by(user_id: session[:user_id])
    @attributes = Profile.attribute_names - %w(id user_id created_at updated_at)
